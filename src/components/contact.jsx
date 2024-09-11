f126c222eb72a711143d0ef9bfaea6985558aae1
kRiSt0ff0 <32828835+kRiSt0ff0@users.noreply.github.com>
2024-09-10 19:35:21 -0600

    content-fix-grammar

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -64,7 +64,7 @@ export const Contact = (props) => {
                         id="name"
                         name="name"
                         className="form-control"
-                        placeholder="Name"
+                        placeholder="Nombre"
                         required
                         onChange={handleChange}
                       />
@@ -78,7 +78,7 @@ export const Contact = (props) => {
                         id="email"
                         name="email"
                         className="form-control"
-                        placeholder="Email"
+                        placeholder="Correo"
                         required
                         onChange={handleChange}
                       />
@@ -92,7 +92,7 @@ export const Contact = (props) => {
                     id="message"
                     className="form-control"
                     rows="4"
-                    placeholder="Message"
+                    placeholder="Mensaje"
                     required
                     onChange={handleChange}
                   ></textarea>
@@ -107,7 +107,7 @@ export const Contact = (props) => {
           </div>
           <div className="col-md-3 col-md-offset-1 contact-info">
             <div className="contact-item">
-              <h3>Informacion de contacto</h3>
+              <h3>Información de contacto</h3>
               <p>
                 <span>
                   <i className="fa fa-map-marker"></i> Dirección
